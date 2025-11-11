6dea50498f8a1888f72d506de2681a994581299d
webguru78 <heyyusman996@gmail.com>
2025-11-11 23:06:07 +0500

    Last Changing

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Breadcrum from '../OverviewComponents/Breadcrum'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import About from '../components/About'
-import Counter from '../components/Counter'
-import Vision from '../components/Vision'
-import ContactForm from '../OverviewComponents/ContactForm'
-
+import React, { useEffect } from "react";
+import Breadcrum from "../OverviewComponents/Breadcrum";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import About from "../components/About";
+import Counter from "../components/Counter";
+import Vision from "../components/Vision";
+import ContactForm from "../OverviewComponents/ContactForm";
 
 const Overview = () => {
+  useEffect(() => {
+    if (window.jQuery) {
+      console.log("✅ jQuery is loaded.");
+
+      window.jQuery(document).ready(function () {
+        console.log("✅ DOM ready, jQuery plugins can run now.");
+
+        if (typeof window.initAnimations === "function") {
+          window.initAnimations();
+        }
+      });
+    } else {
+      console.warn(
+        "❌ jQuery not found. Make sure it's loaded in index.html correctly."
+      );
+    }
+  }, []);
+
   return (
     <div>
-      
-        <Navbar/>
-        <Breadcrum />
-        <About/>
-        <Vision/>
-        <Counter/>
-        <ContactForm/>
-        <Footer/>
+      <Navbar />
+      <Breadcrum />
+      <About />
+      <Vision />
+      <Counter />
+      <ContactForm />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
--- src/pages/WhyChoose.jsx
+++ src/pages/WhyChoose.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import WhyBreadcrumb from '../WhyChoose/WhyBreadcrum'
-import Working from '../components/Working'
-import Clients from '../WhyChoose/Clients'
-import Counter from '../components/Counter'
-import Partner from '../components/Partner'
-
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import WhyBreadcrumb from "../WhyChoose/WhyBreadcrum";
+import Working from "../components/Working";
+import Clients from "../WhyChoose/Clients";
+import Counter from "../components/Counter";
+import Partner from "../components/Partner";
 
 const WhyChoose = () => {
+  useEffect(() => {
+    if (window.jQuery) {
+      console.log("✅ jQuery is loaded.");
+
+      window.jQuery(document).ready(function () {
+        console.log("✅ DOM ready, jQuery plugins can run now.");
+
+        if (typeof window.initAnimations === "function") {
+          window.initAnimations();
+        }
+      });
+    } else {
+      console.warn("❌ jQuery not found. Make sure it's loaded in index.html correctly.");
+    }
+  }, []);
+
   return (
     <div>
-     
-        <Navbar/>
-        <WhyBreadcrumb/>
-        <Working/>
-        <Clients/>
-        <Counter/>
-        <Partner/>
-        <Footer/>
+      <Navbar />
+      <WhyBreadcrumb />
+      <Working />
+      <Clients />
+      <Counter />
+      <Partner />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default WhyChoose
+export default WhyChoose;
